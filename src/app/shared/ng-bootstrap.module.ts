4e895ef2e51ea4880d0cfae9cabb119a65868b2a
Sanjeev <sanjeev.b@gmx.de>
2019-06-08 22:05:09 +0200

    Add shared modules

---
+++ src/app/shared/ng-bootstrap.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from '@angular/core';
+import {NgbTabsetModule} from "@ng-bootstrap/ng-bootstrap";
+
+@NgModule({
+  declarations: [],
+  exports: [NgbTabsetModule]
+})
+export class NgBootstrapModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {NgBootstrapModule} from "./ng-bootstrap.module";
+
+@NgModule({
+  declarations: [],
+  exports: [NgBootstrapModule]
+})
+export class SharedModule {
+}
