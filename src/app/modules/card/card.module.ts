2c2743de40c05baaa4898116f983349392b43c3b
Sanjeev <sanjeev.b@gmx.de>
2019-07-02 18:11:00 +0200

    #1: - Add base form for adding a new card - Add 'CommonModule' to shared.module.ts in order to use structural directives

--- src/app/modules/card/card.module.ts
+++ src/app/modules/card/card.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
+import {AddCardComponent} from './add-card/add-card.component';
 import {CardRoutingModule} from "./card-routing.module";
 import {CardComponent} from './card.component';
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [AddCardComponent, CardComponent],
   imports: [
-    CardRoutingModule
+    CardRoutingModule,
+    SharedModule
   ]
 })
 export class CardModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {NgBootstrapModule} from "./ng-bootstrap.module";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [],
-  exports: [NgBootstrapModule]
+  exports: [NgBootstrapModule, CommonModule]
 })
 export class SharedModule {
 }
