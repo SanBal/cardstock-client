51c8669e5d2256924d5e8b76ce5005381a67394d
Sanjeev <sanjeev.b@gmx.de>
2019-07-02 22:00:28 +0200

    #1: - Add form validation using FormGroup and FormControl

--- src/app/modules/card/add-card/add-card.component.ts
+++ src/app/modules/card/add-card/add-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-card',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-card.component.scss']
 })
 export class AddCardComponent implements OnInit {
+  addCardForm: FormGroup;
+
+  // Options for 'type' and 'rarity' dropdown menus
   types = ["Monster", "Spell", "Trap"];
   rarities = ["Common", "Rare", "Super Rare", "Starfoil", "Super Rare", "Ultra Rare", "Secret Rare"];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    // Initialize form with validators
+    this.addCardForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      type: [this.types[0], Validators.required],
+      rarity: [this.rarities[0], Validators.required]
+    })
+  }
+
+  onSubmit(){
+    console.log("TODO submit");
+    console.log(this.addCardForm)
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {NgBootstrapModule} from "./ng-bootstrap.module";
 import {CommonModule} from "@angular/common";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [],
-  exports: [NgBootstrapModule, CommonModule]
+  exports: [
+    NgBootstrapModule,
+    CommonModule,
+    ReactiveFormsModule]
 })
 export class SharedModule {
 }
