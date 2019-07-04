c1f326474258ad21a3279a25f3edbd531966de98
Sanjeev <sanjeev.b@gmx.de>
2019-07-04 20:18:10 +0200

    #1: - Add list selections for spell and trap card type

--- src/app/modules/card/add-card/add-card.component.ts
+++ src/app/modules/card/add-card/add-card.component.ts
@@ -9,23 +9,50 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class AddCardComponent implements OnInit {
   addCardForm: FormGroup;
 
-  // Options for 'card-type', and 'rarity' dropdown menus
+  // Flags to show/hide corresponding card type infos
+  showMonsterInfos = true;
+  showSpellInfos = false;
+  showTrapInfos: boolean;
+
+  // Options for dropdown menus
   cardTypes = ["Monster", "Spell", "Trap"];
+  spellTypes = ["Ausrüstung", "Normal", "Permanent", "Ritual", "Schnell", "Spielfeld"];
+  trapTypes = ["Konter", ...this.spellTypes.slice(1, 3)];
   rarities = ["Common", "Rare", "Super Rare", "Starfoil", "Super Rare", "Ultra Rare", "Secret Rare"];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     // Initialize form with form controls containing initial values and  validators
     this.addCardForm = this.formBuilder.group({
       name: ['', Validators.required],
       cardType: [this.cardTypes[0], Validators.required],
-      rarity: [this.rarities[0], Validators.required]
+      rarity: [this.rarities[0], Validators.required],
+      spellType: [this.spellTypes[0], Validators.required],
+      trapType: [this.trapTypes[0], Validators.required],
     })
   }
 
   onSubmit(){
     console.log("TODO submit");
     console.log(this.addCardForm)
+  }
+
+  onCardTypeChange(event: Event) {
+    const cardType = (event.target as HTMLSelectElement).value;
+    if (cardType === "Monster") {
+      this.showMonsterInfos = true;
+      this.showSpellInfos = false;
+      this.showTrapInfos = false;
+    } else if (cardType === "Spell") {
+      this.showSpellInfos = true;
+      this.showMonsterInfos = false;
+      this.showTrapInfos = false;
+    }else if (cardType === "Trap") {
+      this.showTrapInfos = true;
+      this.showMonsterInfos = false;
+      this.showSpellInfos = false;
+    }
   }
 }
