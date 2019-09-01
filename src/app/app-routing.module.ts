30a22189a92722c149d5f4f923acdf39aced2291
Sanjeev <sanjeev.b@gmx.de>
2019-09-01 14:18:20 +0200

    #5: - Add dashboard module - Add dashboard routing module - Add dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  {path: 'cards', loadChildren: './modules/card/card.module#CardModule'}
+  {path: 'cards', loadChildren: './modules/card/card.module#CardModule'},
+  {path: 'dashboard', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'},
+  {path: '**', redirectTo: 'dashboard'}
 ];
 
 @NgModule({
